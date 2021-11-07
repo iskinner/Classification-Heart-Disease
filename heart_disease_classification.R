d69c245e684f1c2a6d208fe18262f23e13ee8c48
Ian Skinner <ianskinneruh@gmail.com>
2021-11-07 11:00:57 -0600

    started data exploration

--- heart_disease_classification.R
+++ heart_disease_classification.R
@@ -2,20 +2,46 @@
 #2021-11-07
 #Objective: take heart disease dataset and build classification models
 
-#clean environ
+#setup========================================================================================================================================================================
 rm(list = ls())
+pacman::p_load(tidyverse, janitor, scales, rio, na.tools)
 
-#call pkgs
-pacman::p_load(tidyverse, janitor, scales, rio)
-
-#import dataset
+#import dataset=================================================================================================================================================================================
 data = rio::import("C:/Users/gamin/Downloads/archive/heart.csv") %>% 
   clean_names()
 
+data = data %>% 
+  mutate(across(everything(), ~ifelse(.x == "", NA, .x)),
+         id = row_number())
 
+#exploration========================================================================================================================================================================
 summary(data)
 
+#na check
+nas = anti_join(data, na.omit(data), by = "id")
+na_rows = nrow(nas)
+
+#need to encode these, if we end up using them
+chars = names(data %>% 
+  select(where(is.character)))
+
+chars_sep = 
+
+#count by gender
 data %>% 
   count(sex) %>% 
   mutate(pct = percent(n / sum(n))) %>% 
   arrange(-n)
+
+pivoter = function(varname) {
+  data %>% 
+    count({{varname}}) %>% 
+    mutate(pct = percent(n / sum(n))) %>% 
+    arrange(-n)
+}
+
+pivoter(sex)
+pivoter(chest_pain_type)
+pivoter(resting_ecg)
+pivoter(exercise_angina)
+pivoter(st_slope)
